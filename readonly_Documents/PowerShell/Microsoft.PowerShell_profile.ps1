1095b1c8e11f646211beb366f1794fd4c31901f5
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2024-08-14 21:59:08 +0100

    Update powershell profile

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,57 @@
 oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
+
+# Git
+function gs {
+    git status
+}
+
+function glo {
+    git log --decorate --oneline
+}
+
+# Misc
+function ca {
+    code .
+}
+
+function tab {
+    wt -w 0 nt -d .
+}
+
+function rep {
+    cd ~\repos
+}
+
+function learn {
+    cd ~\learn
+}
+
+function tr {
+    . $PROFILE
+}
+
+function cdf {
+    $release = Get-ItemPropertyValue -LiteralPath 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release
+    switch ($release) {
+        { $_ -ge 533320 } { $version = '4.8.1 or later'; break }
+        { $_ -ge 528040 } { $version = '4.8'; break }
+        { $_ -ge 461808 } { $version = '4.7.2'; break }
+        { $_ -ge 461308 } { $version = '4.7.1'; break }
+        { $_ -ge 460798 } { $version = '4.7'; break }
+        { $_ -ge 394802 } { $version = '4.6.2'; break }
+        { $_ -ge 394254 } { $version = '4.6.1'; break }
+        { $_ -ge 393295 } { $version = '4.6'; break }
+        { $_ -ge 379893 } { $version = '4.5.2'; break }
+        { $_ -ge 378675 } { $version = '4.5.1'; break }
+        { $_ -ge 378389 } { $version = '4.5'; break }
+        default { $version = $null; break }
+    }
+
+    if ($version) {
+        Write-Host -Object ".NET Framework Version: $version"
+    } else {
+        Write-Host -Object '.NET Framework Version 4.5 or later is not detected.'
+    }
+}
