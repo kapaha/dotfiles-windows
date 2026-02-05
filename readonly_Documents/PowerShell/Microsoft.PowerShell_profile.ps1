c67e883c3aa2ea807ca064477135f41549144262
Kai Paterson-Hall <kai.paterson-hall@brandex.co.uk>
2026-02-05 13:30:08 +0000

    Format file

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,53 @@
 oh-my-posh --init --shell pwsh --config ~/my-theme.omp.json | Invoke-Expression
 
+# Modules
+
 Import-Module -Name Terminal-Icons
 Import-Module posh-git
 
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
+# Helpers
+
+function ca {
+    code .
+}
+
+function tab {
+    wt -w 0 nt -d .
+}
+
+function tr {
+    . $PROFILE
+}
+
+function cdf {
+    $release = Get-ItemPropertyValue -LiteralPath 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release
+    switch ($release) {
+        { $_ -ge 533320 } { $version = '4.8.1 or later'; break }
+        { $_ -ge 528040 } { $version = '4.8'; break }
+        { $_ -ge 461808 } { $version = '4.7.2'; break }
+        { $_ -ge 461308 } { $version = '4.7.1'; break }
+        { $_ -ge 460798 } { $version = '4.7'; break }
+        { $_ -ge 394802 } { $version = '4.6.2'; break }
+        { $_ -ge 394254 } { $version = '4.6.1'; break }
+        { $_ -ge 393295 } { $version = '4.6'; break }
+        { $_ -ge 379893 } { $version = '4.5.2'; break }
+        { $_ -ge 378675 } { $version = '4.5.1'; break }
+        { $_ -ge 378389 } { $version = '4.5'; break }
+        default { $version = $null; break }
+    }
+
+    if ($version) {
+        Write-Host -Object ".NET Framework Version: $version"
+    } else {
+        Write-Host -Object '.NET Framework Version 4.5 or later is not detected.'
+    }
+}
+
 # Git
+
 function gs {
     git status
 }
@@ -33,33 +74,6 @@ function glo {
 
 function gfp {
     git fetch -pv
-}
-
-# Chezmoi
-
-function cm {
-    cd ~\.local\share\chezmoi\
-}
-
-function cmc {
-    cd ~\.config\chezmoi
-}
-
-# Misc
-function ca {
-    code .
-}
-
-function tab {
-    wt -w 0 nt -d .
-}
-
-function rep {
-    cd ~\repos
-}
-
-function site {
-    cd ~\repos\sites
 }
 
 function git-mb-in {
@@ -107,47 +121,39 @@ function git-mb {
     git difftool "$base..$TargetBranch"
 }
 
+# Chezmoi
+
+function cm {
+    cd ~\.local\share\chezmoi\
+}
+
+function cmc {
+    cd ~\.config\chezmoi
+}
+
+function rep {
+    cd "~\repos"
+}
+
+function site {
+    cd "~\repos\sites"
+}
+
+# Directories
 
 function prep {
-    cd "C:\Users\KaiPaterson-Hall\personal\repos"
+    cd "~\personal\repos"
 }
 
 function learn {
-    cd ~\learn
+    cd "~\learn"
 }
 
 function cms {
-    cd ~\learn\public-repos\Umbraco-CMS
+    cd "~\learn\public-repos\Umbraco-CMS"
 }
 
-function tr {
-    . $PROFILE
-}
-
-function cdf {
-    $release = Get-ItemPropertyValue -LiteralPath 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release
-    switch ($release) {
-        { $_ -ge 533320 } { $version = '4.8.1 or later'; break }
-        { $_ -ge 528040 } { $version = '4.8'; break }
-        { $_ -ge 461808 } { $version = '4.7.2'; break }
-        { $_ -ge 461308 } { $version = '4.7.1'; break }
-        { $_ -ge 460798 } { $version = '4.7'; break }
-        { $_ -ge 394802 } { $version = '4.6.2'; break }
-        { $_ -ge 394254 } { $version = '4.6.1'; break }
-        { $_ -ge 393295 } { $version = '4.6'; break }
-        { $_ -ge 379893 } { $version = '4.5.2'; break }
-        { $_ -ge 378675 } { $version = '4.5.1'; break }
-        { $_ -ge 378389 } { $version = '4.5'; break }
-        default { $version = $null; break }
-    }
-
-    if ($version) {
-        Write-Host -Object ".NET Framework Version: $version"
-    } else {
-        Write-Host -Object '.NET Framework Version 4.5 or later is not detected.'
-    }
-}
-
+# Setup git autocomplete
 Register-ArgumentCompleter -CommandName git-mb,git-mb-in,git-mb-out `
     -ParameterName TargetBranch `
     -ScriptBlock {
